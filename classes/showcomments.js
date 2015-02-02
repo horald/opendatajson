9d3143b47cb22c309d72a7bf6db9720056e361c6
horald <horst.meyer.koeln@gmail.com>
2015-02-02 21:31:10 +0100

    Changes of Vers 1.00.014

--- classes/showcomments.js
+++ classes/showcomments.js
@@ -1,31 +1,40 @@
     $(function(){
  
-     //var url = 'https://buergerhaushalt.stadt-koeln.de/2015/json/';
-     var url = '../data/opendata.json';
+     var nid = GET('nid');
+     var url = '../data/comments'+nid+'.json';
+     //var url = 'https://buergerhaushalt.stadt-koeln.de/2015/json/comments/1400';
      $.getJSON(url,function(mydata){
         console.log('success');
 
         var ni = document.getElementById('mydiv');
         var newdiv = document.createElement('div');
         var dochtml=''; 
+        var nid = GET('nid');
 
-        dochtml = '<table class="table">';
+        dochtml = 'Nr.:'+nid;
+        dochtml = dochtml + '<h2>'+decodeURI(GET('comment'))+'</h2>';
+
+        dochtml = dochtml + '<table class="table">';
         dochtml = dochtml + '<tr>';
         dochtml = dochtml + '<th>Nr</th>';
         dochtml = dochtml + '<th>Titel</th>';
-        dochtml = dochtml + '<th>Ort</th>';
+        dochtml = dochtml + '<th>Kommentar</th>';
+        dochtml = dochtml + '<th>erzeugt</th>';
+        dochtml = dochtml + '<th>aktualisiert</th>';
         dochtml = dochtml + '</tr>';
 
         var index = 0;
-	mydata.proposals.forEach(function(entry) {
+	mydata.comments.forEach(function(entry) {
           index = index + 1;
         
           lweiter=true;
           if (lweiter) {
             dochtml = dochtml + '<tr>';
             dochtml = dochtml + '<td>'+index+'</td>';
-            dochtml = dochtml + '<td><a href="showdata.html?id='+index+'">'+entry.proposal.title+'</a></td>';
-            dochtml = dochtml + '<td>'+entry.proposal.district+'</td>';
+            dochtml = dochtml + '<td>'+entry.comment.title+'</td>';
+            dochtml = dochtml + '<td>'+entry.comment.Kommentar+'</td>';
+            dochtml = dochtml + '<td>'+entry.comment.created+'</td>';
+            dochtml = dochtml + '<td>'+entry.comment.aktualisiert+'</td>';
             dochtml = dochtml + '</tr>';
           }	
 	});
